0e7f163119b4f4d510f9da336cf6ce4cc37c30a5
NavyaaSharma <navyaasharma700@rediffmail.com>
2020-06-20 18:37:55 +0530

    api linking

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 
+
 (function ($) {
     "use strict";
 
@@ -19,186 +20,127 @@
 
 })(jQuery);
 
-
-function Validateemail() {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById("email").value)) {
-        return true;
+function register()
+{
+    var data={
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value
     }
-    ShowToast("Please enter a valid email address.");
-    return false;
-}
-
-function ValidatePass() {
-    if (document.getElementById("password").value.length > 5)
-        return true;
-    else
-        ShowToast("Wrong password");
-    return false;
-
-}
-
-function ValidatePassSignup() {
-    if (document.getElementById("password").value.length > 5)
-        return true;
-    else
-        ShowToast("Enter minimum 6 Characters");
-    return false;
-
-}
-
-
-function Login() {
-    if (!Validateemail()) { } else {
-        if (!ValidatePass()) { } else {
-            document.getElementById("signin").disabled = true
-            let fetchData = {
-                method: 'POST',
-                body: JSON.stringify({
-                    "username": document.getElementById("email").value,
-                    "password": document.getElementById("password").value
-                }),
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            }
-            fetch(LOGIN_URL, fetchData)
-                .then(function (res) {
-                    if (res.status == 401) {
-                        ShowToast("Wrong credentials!")
-                        document.getElementById("password").value = ''
-                        document.getElementById("signin").disabled = false
-                    } else if (res.status == 200) {
-                        res.json().then(function (data) {
-                            if (data == null) { } else {
-                                ShowToast("Login Success")
-                                localStorage.setItem("TOKEN", "Token " + data.user.token)
-                                localStorage.setItem("FULL_NAME", data.user.full_name)
-                                document.getElementById("signin").disabled = false
-                                return window.location.replace("/");
-                            }
-                        })
-                    } else {
-                        ShowToast("Server error. Our team has been informed. Working on it.");
-                        document.getElementById("email").value = '';
-                        document.getElementById("password").value = '';
-                        document.getElementById("signin").disabled = false
-                    }
-                    return;
-                })
-                .catch((error) => { })
+    var xh = new XMLHttpRequest();
+    xh.open("POST", "https://api-linking.herokuapp.com/users", true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.send(JSON.stringify(data))
+    xh.onload=function(){
+        if(this.status==201)
+        {
+            alert('registered successfully! Login to continue')
+            window.location.replace('signin.html')
         }
-    }
-}
-
-function SignUp() {
-    if (!Validateemail()) { } else {
-        if (!ValidatePassSignup()) { } else {
-            document.getElementById("signup").disabled = true
-            let fetchData = {
-                method: 'POST',
-                body: JSON.stringify({
-                    "username": document.getElementById("email").value,
-                    "password": document.getElementById("password").value,
-                    "phone":document.getElementById("phone").value,
-                    "full_name":document.getElementById("name").value,
-                }),
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            }
-            fetch(REGISTER_URL, fetchData)
-                .then(function (res) {
-                    console.log(res.status);
-                    console.log(res.json)
-                    if (res.status == 400) {
-                        ShowToast("User Already exists!")
-                        document.getElementById("password").value = ''
-                        document.getElementById("signup").disabled = false
-                    } else if (res.status == 201) {
-                        res.json().then(function (data) {
-                            if (data == null) { } else {
-                                ShowToast("Login Success")
-                                localStorage.setItem("TOKEN", "Token " + data.user.token)
-                                localStorage.setItem("FULL_NAME", data.user.full_name)
-                                document.getElementById("signup").disabled = false
-                                return window.location.replace("/signin/");
-                            }
-                        })
-                    } else {
-                        ShowToast("Server error. Our team has been informed. Working on it.");
-                        document.getElementById("email").value = '';
-                        document.getElementById("password").value = '';
-                        document.getElementById("signup").disabled = false
-                    }
-                    return;
-                })
-                .catch((error) => { })
-
-
+        else{
+            alert('Failed! Try again')
+            window.location.replace('signup.html')
         }
-    }
-
-
+}
 }
 
-function gup(name, url) {
-    if (!url) url = location.href;
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\?&]" + name + "=([^&#]*)";
-    var regex = new RegExp(regexS);
-    var results = regex.exec(url);
-    return results == null ? null : results[1];
+function login()
+{
+    var data=
+    {
+	"email":document.getElementById('email').value,
+	"password":document.getElementById('password').value
+    }
+    var xh = new XMLHttpRequest();
+    xh.open("POST", "https://api-linking.herokuapp.com/users/login", true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.send(JSON.stringify(data))
+    xh.onload=function(){
+        if(this.status==200)
+        {
+            var data = JSON.parse(this.responseText)
+            localStorage.setItem("JWT_Token", "JWT " + data.token)
+            window.location.replace('index.html')
+        }
+        else{
+            alert('Invalid login credentials')
+            window.location.replace('signin.html')
+        }
+}
 }
 
+function getall()
+{
+    var jwt = localStorage.getItem('JWT_Token')
+    console.log(jwt)
+    var xh = new XMLHttpRequest();
+    xh.open("GET", "https://api-linking.herokuapp.com/all/items", true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.setRequestHeader('Authorization', jwt)
+    xh.send()
+    xh.onload=function(){
+        if(this.status==200)
+        {
+            var data = JSON.parse(this.responseText)
 
-
-function Linkedin() {
-    let fetchData = {
-        method: 'POST',
-        body: JSON.stringify({
-            "code":gup('code', window.location.href)
-        }),
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }
-    fetch(LINKEDIN_URL, fetchData)
-        .then(function (res) {
-            if (res.status == 400) {
-                ShowToast("Authentication Failed! Redirecting to login...")
-                setTimeout(function() {
-                    return window.location.replace("/signin/");
-                  }, 5000);
-            } else if (res.status == 403) {
-                ShowToast("Authentication code missing! Redirecting to login...")
-                setTimeout(function() {
-                    return window.location.replace("/signin/");
-                  }, 5000);
-            } else if (res.status == 206) {
-                ShowToast("Account not authorised. You can authroise account in settings after logging in! Redirecting to login...")
-                setTimeout(function() {
-                    return window.location.replace("/signin/");
-                  }, 5000);
-            } else if (res.status == 200) {
-                res.json().then(function (data) {
-                    if (data == null) { } else {
-                        ShowToast("Login Success")
-                        localStorage.setItem("TOKEN", "Token " + data.user.token)
-                        localStorage.setItem("FULL_NAME", data.user.full_name)
-                        return window.location.replace("/");
-                    }
-                })
-            }else {
-                ShowToast("Server error. Our team has been informed. Working on it.");
-                document.getElementById("email").value = '';
-                document.getElementById("password").value = '';
+            for(var i=0;i<data.payload.length;i++)
+            {
+                $('#allItems').append(`<li><span id="item">${data.payload[i].name}</span>&nbsp;&nbsp;&nbsp;&nbsp;<button class="waves-effect btn red" type="submit" id="delete${data.payload[i]._id}" name="${data.payload[i]._id}" onclick="deleteitem(this.name)">Remove</button></li>`)
             }
-            return;
-        })
-        .catch((error) => { })
+        }
+        else if(this.status==400){
+            alert('Error in getting items')
+        }
+        else if(this.status==401){
+            alert('Please authenticate user')
+        }
+}
 }
 
-// console.clear();
+function newItem()
+{
+    var data={
+        name:document.getElementById('name').value,
+        description:document.getElementById('desc').value,
+        qty:document.getElementById('qty').value
+    }
+    console.log(data)
+    var jwt = localStorage.getItem('JWT_Token')
+    var xh = new XMLHttpRequest();
+    xh.open("POST", "https://api-linking.herokuapp.com/add/item", true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.setRequestHeader('Authorization', jwt)
+    xh.send(JSON.stringify(data))
+    xh.onload=function(){
+        if(this.status==201)
+        {
+            alert('Item added!')
+            window.location.replace('index.html')
+        }
+        else{
+            alert('Failed! Try again')
+            window.location.replace('index.html')
+        }
+}
+}
+
+function deleteitem(id)
+{
+    var jwt = localStorage.getItem('JWT_Token')
+    var xh = new XMLHttpRequest();
+    xh.open("DELETE", `https://api-linking.herokuapp.com/delete/item/${id}`, true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.setRequestHeader('Authorization', jwt)
+    xh.send()
+    xh.onload=function(){
+        if(this.status==200)
+        {
+            alert('Item deleted!')
+            window.location.replace('index.html')
+        }
+        else{
+            alert('Failed! Try again')
+            window.location.replace('index.html')
+        }
+}
+}
